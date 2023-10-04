1fcc9d36613fa9aa52c79b19bb1e7bd503548409
Nimesh Asanka <jnimeshasanka@gmail.com>
2023-10-04 16:32:47 +0530

    Enable CORS for back-end

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const issues = require('./routes/issues');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/issues', issues);
