cd91936dffe7a6418204c7af36ae7aa6ce1da249
Nimesh Asanka <jnimeshasanka@gmail.com>
2023-10-04 16:14:28 +0530

    Initial commit for front-end app

--- back-end/index.js
+++ back-end/index.js
@@ -8,6 +8,6 @@ app.use(express.json());
 app.use('/api/issues', issues);
 
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Server running on port ' + (process.env.PORT || 3000) + '...');
+app.listen(process.env.PORT || 4000, () => {
+    console.log('Server running on port ' + (process.env.PORT || 4000) + '...');
 });
